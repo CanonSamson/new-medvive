37383aee7c6484ff26bdd1c90f38ed4f2ed96152
Canon Samson <100407107+CanonSamson@users.noreply.github.com>
2023-12-20 04:57:57 +0000

    fixed

--- app/patient/consultations/page.jsx
+++ app/patient/consultations/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { usePatient } from "../Context";
 import { MdDateRange } from "react-icons/md";
 import Consult from "./Consult";
